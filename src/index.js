4f27ae98195f0e7e0c1b3a953e228fcd0859ef31
Hentix <iu3071pc@gmail.com>
2025-05-29 08:57:28 +0545

    Connection made with mongoDB

---
+++ src/index.js
@@ -0,0 +1,45 @@
+// require('dotenv').config({path: './env'})
+
+
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+
+// database sanga connect gardda jaila try catch use garne
+// database ko lagi time ni lagxa so async await
+
+
+dotenv.config({
+    path: './env'
+})
+
+
+connectDB()
+
+
+
+
+
+
+
+// Type 1 : To connect database in index.js
+
+/*
+(async() => {
+    try{
+        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_Name}`)
+        app.on("error", (error) => {
+            console.log("ERROR: ", error);
+            throw error
+        })
+
+        app.listen(porocess.env.PORT, () => {
+            console.log(`App is listining in port ${process.env.PORT}`)
+        })
+    }
+    catch(error){
+        console.error("Error", error)
+        throw err
+    }
+})()
+*/
